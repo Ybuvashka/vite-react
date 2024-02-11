3d960c728a6ac71793d330d270cc90fcebf7b4ae
usec <ilkivaoleksandra2@gmail.com>
2024-02-11 21:51:48 +0200

    create jwt setup

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,45 +1,76 @@
-import { body, param, validationResult } from 'express-validator'
-import { BadRequestError, NotFoundError } from '../errors/customErrors.js'
-import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js'
-import mongoose from 'mongoose'
-import Job from '../models/JobModel.js'
+import { body, param, validationResult } from "express-validator";
+import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
+import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
+import mongoose from "mongoose";
+import Job from "../models/JobModel.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
   return [
     validateValues,
     (req, res, next) => {
-      const errors = validationResult(req)
+      const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map((error) => error.msg)
+        const errorMessages = errors.array().map((error) => error.msg);
 
-        if(errorMessages[0].startsWith('no job')){
-          throw new NotFoundError(errorMessages)
+        if (errorMessages[0].startsWith("no job")) {
+          throw new NotFoundError(errorMessages);
         }
-        throw new BadRequestError(errorMessages)
+        throw new BadRequestError(errorMessages);
       }
-      next()
+      next();
     },
-  ]
-}
+  ];
+};
 
 export const validateJobInput = withValidationErrors([
-  body('company').notEmpty().withMessage('company is required'),
-  body('position').notEmpty().withMessage('position is required'),
-  body('jobLocation').notEmpty().withMessage('job location is required'),
-  body('jobStatus')
+  body("company").notEmpty().withMessage("company is required"),
+  body("position").notEmpty().withMessage("position is required"),
+  body("jobLocation").notEmpty().withMessage("job location is required"),
+  body("jobStatus")
     .isIn(Object.values(JOB_STATUS))
-    .withMessage('invalid status value'),
-  body('jobType')
+    .withMessage("invalid status value"),
+  body("jobType")
     .isIn(Object.values(JOB_TYPE))
-    .withMessage('invalid type value'),
-])
+    .withMessage("invalid type value"),
+]);
 
 export const validateIdParam = withValidationErrors([
-  param('id').custom(async (value) => {
-    const isValidId = mongoose.Types.ObjectId.isValid(value)
-    if (!isValidId) throw new Error('invalid mongodb id')
+  param("id").custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new Error("invalid mongodb id");
 
-    const job = await Job.findById(value)
-    if (!job) throw new NotFoundError(`no job with that id ${value}`)
+    const job = await Job.findById(value);
+    if (!job) throw new NotFoundError(`no job with that id ${value}`);
   }),
-])
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body("name").notEmpty().withMessage("name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format")
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError("email already exist");
+      }
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 8 })
+    .withMessage("password must be at least 8 characters long"),
+  body("location").notEmpty().withMessage("location is required"),
+]);
+
+export const validateLoginInput = withValidationErrors([
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format"),
+  body("password").notEmpty().withMessage("password is required"),
+]);
--- server.js
+++ server.js
@@ -3,24 +3,32 @@ import morgan from "morgan";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import "express-async-errors";
+import cookieParser from "cookie-parser";
+
+//middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import {authenticateUser} from "./middleware/authMiddleware.js";
 
 dotenv.config();
 const app = express();
 
 //routers
 import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(cookieParser())
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs",authenticateUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
